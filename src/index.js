8a680389c571bce0fad1a3b62439bdf9447ad0d7
Caio Mantovani <caiovmborba@gmail.com>
2020-05-09 13:37:43 -0300

    Configure Nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.json({message: 'Hello World!'});
+  return res.json({ message: 'Hello World!' });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
